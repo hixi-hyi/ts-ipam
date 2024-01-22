9d1d995561c5925e82180cd11b9e7db11ab827a2
Hiroyoshi Houchi <git@hixi-hyi.com>
2024-01-22 14:19:38 +0900

    v0.0.2

--- example/ipam.ts
+++ example/ipam.ts
@@ -1,17 +1,52 @@
-import { NetworkAddress } from './../src/ipam';
-import { table } from 'table';
 import * as ipam from '../src/ipam';
+import * as ipNum from 'ip-num';
+
+let manager;
+manager = new ipam.ReservedManager(ipam.NETWORK_BLOCK_10, 8, 24, { addressFormat: ipam.ADDRESS_FORMAT_RANGE });
+manager.reserve('10.0.0.0/16', 'production for hixi', 'prod-hixi');
+manager.reserve('10.1.0.0/24', 'development for hixi', 'dev-hixi');
+manager.reserve('10.1.1.0/24', 'development for hixi2', 'dev-hixi2');
+manager.printTable();
+manager.printCsv();
+console.log(manager.getReserve('dev-hixi').cidr);
+//
+//manager = new ipam.SummaryPoolManager({ block: ipam.BLOCK_10, start: 8, end: 24, addressFormat: ipam.ADDRESS_FORMAT_RANGE });
+//manager.reserve('10.0.0.0/10', 'development for hixi', 'dev-hixi');
+//manager.reserve('10.128.0.0/10', 'development for hixi', 'prod-hixi2');
+//manager.printTable();
 
 //const block = new ipam.Block10();
 //const manager = new ipam.Manager({ blocks: [block, new ipam.Block172()], start: 8, end: 10 });
-const networkAddress = ipam.CollectNetworkAddresses('10.0.0.0', 10, 13);
-const smallestNetworkAddress = ipam.findLongestNetworkAddress(networkAddress);
-const reserves = [ipam.NetworkAddress.reserve('10.0.0.0', 11, 'dev')];
-for (const reserve of reserves) {
-  for (const na of smallestNetworkAddress) {
-    if (reserve.range.contains(na.range)) {
-      networkAddress.push(ipam.NetworkAddress.reserve(na.address, na.prefix, 'dev'));
-    }
-  }
-}
-ipam.PrintTable(networkAddress);
+//const networkAddress = ipam.CollectNetworkAddresses('10.0.0.0', 10, 13);
+//const smallestNetworkAddress = ipam.findLongestNetworkAddress(networkAddress);
+//const reserves = [ipam.NetworkAddress.reserve('10.0.0.0', 11, 'dev')];
+//for (const reserve of reserves) {
+//  for (const na of smallestNetworkAddress) {
+//    if (reserve.range.contains(na.range)) {
+//      networkAddress.push(ipam.NetworkAddress.reserve(na.address, na.prefix, 'dev'));
+//    }
+//  }
+//}
+////ipam.PrintTable(networkAddress);
+////
+//function print(msg: string, pool) {
+//  console.log(msg);
+//  for (const range of pool.getRanges()) {
+//    console.log(range.toCidrRange().toRangeString());
+//  }
+//  console.log('---');
+//}
+//
+//const block = ipam.CidrRange('10.0.0.0', 10);
+//let pool = ipNum.Pool.fromCidrRanges([block]);
+//print('original', pool);
+//let removed;
+//removed = pool.removeOverlapping(ipNum.RangedSet.fromCidrRange(ipNum.IPv4CidrRange.fromCidr('10.16.0.0/12')));
+//console.log(removed);
+//removed = pool.removeOverlapping(ipNum.RangedSet.fromCidrRange(ipNum.IPv4CidrRange.fromCidr('10.32.0.0/12')));
+//console.log(removed);
+//removed = pool.removeOverlapping(ipNum.RangedSet.fromCidrRange(ipNum.IPv4CidrRange.fromCidr('10.40.0.0/13')));
+//console.log(removed);
+//let aggregated = pool.aggregate();
+////pool.removeExact(ipNum.RangedSet.fromCidrRange(ipNum.IPv4CidrRange.fromCidr('10.48.0.0/13')));
+//print('remove', aggregated);
--- src/ipam.ts
+++ src/ipam.ts
@@ -1,33 +1,73 @@
+import { IPv4CidrRange } from 'ip-num/IPRange';
 import * as ipNum from 'ip-num';
 import { table } from 'table';
 
 const MIN_CIDR_PREFIX = 8; // 10.0.0.0/8
 const MAX_CIDR_PREFIX = 24; // 192.168.0.0/24
 
+export class NetworkBlock {
+  public readonly range: ipNum.IPv4CidrRange;
+  constructor(ip: string, prefix: number) {
+    this.range = CidrRange(ip, prefix);
+  }
+}
+
+export const NETWORK_BLOCK_10 = new NetworkBlock('10.0.0.0', 8);
+export const NETWORK_BLOCK_172 = new NetworkBlock('172.16.0.0', 12);
+export const NETWORK_BLOCK_192 = new NetworkBlock('192.168.0.0', 16);
+
+export class NetworkAddress {
+  range: ipNum.IPv4CidrRange;
+  address: string;
+  prefix: number;
+  label: string;
+  code?: string;
+
+  public constructor(range: ipNum.IPv4CidrRange, label: string, code?: string) {
+    this.range = range;
+    this.address = range.getFirst().toString();
+    this.prefix = Number(range.cidrPrefix.getValue());
+    this.label = label;
+    this.code = code;
+  }
+  public get cidr(): string {
+    return this.range.toCidrString();
+  }
+}
+
 export function CidrRange(address: string, prefix: number): ipNum.IPv4CidrRange {
   const range = new ipNum.IPv4CidrRange(ipNum.IPv4.fromDecimalDottedString(address), ipNum.IPv4Prefix.fromNumber(BigInt(prefix)));
   return range;
 }
-
-export interface ManagerProps {
-  start: number;
-  end: number;
-  blocks: Block[];
+export function CidrRangeFromCidr(cidr: string): ipNum.IPv4CidrRange {
+  return ipNum.IPv4CidrRange.fromCidr(cidr);
 }
-export class Manager {
-  public readonly blocks: Block[];
+export function CidrStringToIpAndPrefix(cidr: string): [string, number] {
+  const [address, prefix] = cidr.split('/');
+  return [address, Number(prefix)];
+}
+
+type AddressFormat = 'CIDR' | 'RANGE';
+export const ADDRESS_FORMAT_CIDR: AddressFormat = 'CIDR';
+export const ADDRESS_FORMAT_RANGE: AddressFormat = 'RANGE';
+
+export interface ManagerConfigProps {
+  addressFormat?: AddressFormat;
+}
+abstract class AbstractManager {
+  public readonly block: NetworkBlock;
   public readonly start: number;
   public readonly end: number;
-  public readonly networkAddress: NetworkAddress[] = [];
-  public readonly reservedAddress: NetworkAddress[] = [];
+  public readonly reserved: NetworkAddress[] = [];
+  public readonly addressFormat: AddressFormat = ADDRESS_FORMAT_CIDR;
 
-  constructor(props: ManagerProps) {
-    this.blocks = props.blocks;
-    this.start = props.start;
-    this.end = props.end;
+  constructor(block: NetworkBlock, start: number, end: number, config: ManagerConfigProps = {}) {
+    this.block = block;
+    this.start = start;
+    this.end = end;
+    this.addressFormat = config.addressFormat ?? ADDRESS_FORMAT_CIDR;
     this.validate();
   }
-
   private validate() {
     if (this.start > this.end) {
       throw new Error('start must be less than or equal to end');
@@ -40,89 +80,173 @@ export class Manager {
       throw new Error(`End must be between ${MIN_CIDR_PREFIX} and ${MAX_CIDR_PREFIX}, inclusive.`);
     }
   }
-}
-
-export class Block {
-  public readonly range: ipNum.IPv4CidrRange;
-  constructor(ip: string, prefix: number) {
-    this.range = CidrRange(ip, prefix);
-  }
-}
-
-export const BLOCK_10 = new Block('10.0.0.0', 8);
-export const BLOCK_172 = new Block('172.31.0.0', 12);
-export const BLOCK_192 = new Block('192.168.0.0', 16);
-
-export class NetworkAddress {
-  range: ipNum.IPv4CidrRange;
-  address: string;
-  prefix: number;
-  label?: string;
-  reserved?: boolean;
-
-  public constructor(range: ipNum.IPv4CidrRange, label?: string) {
-    this.range = range;
-    this.address = range.getFirst().toString();
-    this.prefix = Number(range.cidrPrefix.getValue());
-    this.label = label;
-  }
-  public static reserve(address: string, prefix: number, label?: string): NetworkAddress {
-    const na = new NetworkAddress(CidrRange(address, prefix), label);
-    na.reserved = true;
-    return na;
-  }
-}
-
-export function CollectNetworkAddresses(networkAddress: string, minCidrPrefix: number, maxCidrPrefix: number): NetworkAddress[] {
-  const start = CidrRange(networkAddress, minCidrPrefix);
-  const result = [];
-  for (let i = maxCidrPrefix; i >= minCidrPrefix; i--) {
-    result.push(...start.splitInto(new ipNum.IPv4Prefix(BigInt(i))).map((range) => new NetworkAddress(range)));
-  }
-  return result;
-}
-
-export function findLongestNetworkAddress(addresses: NetworkAddress[]): NetworkAddress[] {
-  if (addresses.length === 0) {
-    return [];
-  }
-
-  let longestPrefix = addresses[0].prefix;
-  for (const ip of addresses) {
-    if (ip.prefix > longestPrefix) {
-      longestPrefix = ip.prefix;
-    }
-  }
-
-  return addresses.filter((ip) => ip.prefix === longestPrefix);
-}
-
-export function PrintTable(networkAddresses: NetworkAddress[]) {
-  // 利用されているすべてのプレフィックスを抽出
-  const prefixes = Array.from(new Set(networkAddresses.map((entry) => entry.prefix)))
-    .sort((a, b) => a - b) // プレフィックスを昇順に並べ替え
-    .map((prefix) => `/${prefix}`);
-  prefixes.push('reserved');
-
-  // IPアドレスごとに行を作成
-  const ipRows: Record<string, string[]> = {};
-  networkAddresses.forEach((entry) => {
-    const rowKey = entry.address;
-    if (!ipRows[rowKey]) {
-      ipRows[rowKey] = new Array(prefixes.length).fill('');
-    }
-    if (entry.reserved) {
-      ipRows[rowKey][prefixes.length - 1] = entry.label || '';
+  protected formatAddress(range: ipNum.IPv4CidrRange): string {
+    if (this.addressFormat === ADDRESS_FORMAT_CIDR) {
+      return range.toCidrString();
     } else {
-      const prefixIndex = prefixes.indexOf(`/${entry.prefix}`);
-      ipRows[rowKey][prefixIndex] = entry.address;
+      return range.toRangeString();
     }
-  });
-
-  // 行データを配列に変換
-  const rows = Object.values(ipRows);
-
-  // 表を出力
-  const output = table([prefixes, ...rows]);
-  console.log(output);
+  }
+  public reserve(cidr: string, label: string, code?: string): boolean {
+    const [address, prefix] = CidrStringToIpAndPrefix(cidr);
+    const range = CidrRange(address, prefix);
+    const networkAddress = range.getFirst().toString();
+    if (networkAddress !== address) {
+      throw new Error(`The address ${address}/${prefix} is not a valid network address. Maybe you meant ${networkAddress}/${prefix}?`);
+    }
+    if (prefix < this.start || prefix > this.end) {
+      throw new Error(`The prefix ${prefix} is not between ${this.start} and ${this.end}, inclusive.`);
+    }
+    if (code && this.hasReserve(code)) {
+      throw new Error(`The code ${code} is already reserved.`);
+    }
+    this.reserved.push(new NetworkAddress(range, label, code));
+    return true;
+  }
+  public getReserve(code: string): NetworkAddress | undefined {
+    if (code === '') {
+      return undefined;
+    }
+    return this.reserved.find((entry) => entry.code === code);
+  }
+  public hasReserve(code: string): boolean {
+    if (code === '') {
+      return false;
+    }
+    return this.getReserve(code) !== undefined;
+  }
+  public printCsv(): void {
+    const csv = this.getContents()
+      .map((row) => row.join(','))
+      .join('\n');
+    console.log(csv);
+  }
+  abstract printTable(): void;
+  protected abstract getContents(): string[][];
 }
+
+export class ReservedManager extends AbstractManager {
+  public readonly pool: ipNum.Pool<ipNum.RangedSet<ipNum.IPv4>>;
+  constructor(block: NetworkBlock, start: number, end: number, config: ManagerConfigProps = {}) {
+    super(block, start, end, config);
+    this.pool = ipNum.Pool.fromCidrRanges([this.block.range]);
+  }
+  public reserve(cidr: string, label: string, code?: string): boolean {
+    super.reserve(cidr, label, code);
+    const isReserved = this.pool.removeOverlapping(ipNum.RangedSet.fromCidrRange(CidrRangeFromCidr(cidr)));
+    if (!isReserved) {
+      throw new Error(`Failed to allocate the address range ${cidr}.`);
+    }
+    return isReserved;
+  }
+  public printTable(): void {
+    const config = {
+      header: {
+        content: `Reserved IP Address ${this.block.range.toCidrString()}`,
+      },
+    };
+    console.log(table(this.getContents(), config));
+  }
+  protected getContents(): string[][] {
+    const header = ['address', 'label', 'code'];
+    const rows = this.reserved
+      .sort((a, b) => a.address.localeCompare(b.address))
+      .map((entry) => [this.formatAddress(entry.range), entry.label, entry.code]);
+    return [header, ...rows];
+  }
+}
+//export class SummaryPoolManager extends AbstractManager {
+//  public readonly pool: ipNum.Pool<ipNum.RangedSet<ipNum.IPv4>>;
+//  constructor(props: ManagerProps) {
+//    super(props);
+//    this.pool = ipNum.Pool.fromCidrRanges([this.block.range]);
+//  }
+//  public reserve(cidr: string, label: string, code?: string): boolean {
+//    super.reserve(cidr, label, code);
+//    const isReserved = this.pool.removeOverlapping(ipNum.RangedSet.fromCidrRange(CidrRangeFromCidr(cidr)));
+//    if (this.throwException && !isReserved) {
+//      throw new Error(`Failed to allocate the address range ${cidr}.`);
+//    }
+//    return isReserved;
+//  }
+//  public printTable(): void {
+//    const config = {
+//      header: {
+//        content: `Reserved IP Address ${this.block.range.toCidrString()}`,
+//      },
+//    };
+//    console.log(table(this.getContents(), config));
+//  }
+//  protected getContents(): string[][] {
+//    const header = ['address', 'label', 'code'];
+//    const rows = [];
+//
+//    for (const rs of this.pool.getRanges()) {
+//      const rangeSet = rs as ipNum.RangedSet<ipNum.IPv4>;
+//      console.log(rangeSet.getSize());
+//      const available = rangeSet.toCidrRange() as ipNum.IPv4CidrRange;
+//      //console.log(available);
+//      console.log(available.toRangeString());
+//      console.log(this.formatAddress(available));
+//      rows.push([this.formatAddress(available), 'available', '']);
+//    }
+//    rows.push(
+//      ...this.reserved.sort((a, b) => a.address.localeCompare(b.address)).map((entry) => [this.formatAddress(entry.range), entry.label, entry.code])
+//    );
+//    return [header, ...rows];
+//  }
+//}
+//
+//// export class CompletePoolManager extends AbstractManager {
+//
+//
+//export function CollectNetworkAddresses(networkAddress: string, minCidrPrefix: number, maxCidrPrefix: number): NetworkAddress[] {
+//  const start = CidrRange(networkAddress, minCidrPrefix);
+//  const result = [];
+//  for (let i = maxCidrPrefix; i >= minCidrPrefix; i--) {
+//    result.push(...start.splitInto(new ipNum.IPv4Prefix(BigInt(i))).map((range) => new NetworkAddress(range)));
+//  }
+//  return result;
+//}
+//
+//export function findLongestNetworkAddress(addresses: NetworkAddress[]): NetworkAddress[] {
+//  if (addresses.length === 0) {
+//    return [];
+//  }
+//
+//  let longestPrefix = addresses[0].prefix;
+//  for (const ip of addresses) {
+//    if (ip.prefix > longestPrefix) {
+//      longestPrefix = ip.prefix;
+//    }
+//  }
+//
+//  return addresses.filter((ip) => ip.prefix === longestPrefix);
+//}
+//
+//export function PrintTable(networkAddresses: NetworkAddress[]) {
+//  const prefixes = Array.from(new Set(networkAddresses.map((entry) => entry.prefix)))
+//    .sort((a, b) => a - b) // プレフィックスを昇順に並べ替え
+//    .map((prefix) => `/${prefix}`);
+//  prefixes.push('reserved');
+//
+//  const ipRows: Record<string, string[]> = {};
+//  networkAddresses.forEach((entry) => {
+//    const rowKey = entry.address;
+//    if (!ipRows[rowKey]) {
+//      ipRows[rowKey] = new Array(prefixes.length).fill('');
+//    }
+//    if (entry.reserved) {
+//      ipRows[rowKey][prefixes.length - 1] = entry.label || '';
+//    } else {
+//      const prefixIndex = prefixes.indexOf(`/${entry.prefix}`);
+//      ipRows[rowKey][prefixIndex] = entry.address;
+//    }
+//  });
+//
+//  const rows = Object.values(ipRows);
+//
+//  const output = table([prefixes, ...rows]);
+//  console.log(output);
+//}
+//
